0dcd07b4d893273c27ace7d97ab324777f47867f
Haochen Zhang <hz112@rice.edu>
2026-04-25 08:15:09 +0800

    0425

--- train_all.sh
+++ train_all.sh
@@ -1,2 +1,2 @@
-python train_unconditional.py --dataset "sine" --output_dir "./outputs/sine_0424"
-python train_unconditional.py --dataset "glucose" --output_dir "./outputs/glucose"
+python train_ts_unconditional.py --dataset "sine" --output_dir "./outputs/sine_0424"
+python train_ts_unconditional.py --dataset "glucose" --output_dir "./outputs/glucose"
