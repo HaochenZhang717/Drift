8538746d5b7903ffebb8067a9b6d08b57308594d
Haochen Zhang <hz112@rice.edu>
2026-04-25 20:03:36 +0800

    0425

--- fid_train_all.sh
+++ fid_train_all.sh
@@ -1,6 +1,7 @@
 export CUDA_VISIBLE_DEVICES=5
-bash fid_synth_u.sh
-bash fid_synth_m.sh
-bash fid_ettm1.sh
-bash fid_istanbul_traffic.sh
-bash fid_weather.sh
+#bash fid_synth_u.sh
+#bash fid_synth_m.sh
+#bash fid_ettm1.sh
+#bash fid_istanbul_traffic.sh
+#bash fid_weather.sh
+bash fid_glucose.sh
