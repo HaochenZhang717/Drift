c65a75701e323dd7b37c9d4bdf65b31a1c92c073
Haochen Zhang <hz112@rice.edu>
2026-05-01 18:11:58 +0800

    0501

--- scripts/submit_benchmark_drift.sh
+++ scripts/submit_benchmark_drift.sh
@@ -1,6 +1,14 @@
-CUDA_VISIBLE_DEVICES=1 \
+#CUDA_VISIBLE_DEVICES=1 \
+#REL_PATH=TSF/ETT-small/ETTm1.csv \
+#DATA_BACKEND=ETTm1 \
+#DATASET_NAME=ETTm1 \
+#IN_CHANNEL=7 \
+#bash scripts/benchmark_drift.sh
+
+
+CUDA_ENV=vlm \
 REL_PATH=TSF/ETT-small/ETTm1.csv \
 DATA_BACKEND=ETTm1 \
 DATASET_NAME=ETTm1 \
 IN_CHANNEL=7 \
-bash scripts/benchmark_drift.sh
+sbatch scripts/benchmark_drift.sh
