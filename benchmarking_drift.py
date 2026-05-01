ab6ab3d7d12e8646126c11ad270923bf088e4784
Haochen Zhang <hz112@rice.edu>
2026-05-01 22:23:27 +0800

    0501

--- benchmarking_drift.py
+++ benchmarking_drift.py
@@ -406,7 +406,9 @@ def train(
     config["metric_iteration"] = metric_iteration
     config["ts_feature_encoder_ckpt"] = ts_feature_encoder_ckpt
     config["train_batch_size"] = batch_size
-    config["dataset"] = config["dataset_name"]
+    # config["dataset"] = config["dataset_name"]
+    config["dataset"] = config["data"]
+
     eval_splits = eval_splits or ["train", "test"]
     if ts_feature_encoder_ckpt is not None:
         config["use_feature_encoder"] = True
