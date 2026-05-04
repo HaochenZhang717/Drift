164727ef623542840beb542130aacf8bc6c1255c
Haochen Zhang <hz112@rice.edu>
2026-05-04 20:01:22 +0800

    0503

--- baselines/ImagenFew/scripts/run_imagentime_glucose.sh
+++ baselines/ImagenFew/scripts/run_imagentime_glucose.sh
@@ -5,6 +5,6 @@ ROOT_DIR="/playpen-shared/haochenz/Drift"
 CONFIG_FILE="${CONFIG_FILE:-${ROOT_DIR}/baselines/ImagenFew/configs/ImagenTime/glucose.yaml}"
 SUBSET_P="${SUBSET_P:-1.0}"
 
-python "${ROOT_DIR}/baselines/ImagenFew/run.py" \
+python /playpen-shared/haochenz/Drift/baselines/ImagenFew/run.py \
   --subset_p "${SUBSET_P}" \
   --config "${CONFIG_FILE}"
