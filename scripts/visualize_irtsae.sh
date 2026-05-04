22699a833bc8f512738b3c571f6657f2b9948df1
Haochen Zhang <hz112@rice.edu>
2026-05-04 08:28:38 +0800

    0503

--- scripts/visualize_irtsae.sh
+++ scripts/visualize_irtsae.sh
@@ -1,19 +1,19 @@
-python scripts/visualize_irtsae_without_input_mask_imputed.py \
+python visualize_irtsae_without_input_mask_imputed.py \
   --ckpt_path /playpen-shared/haochenz/Drift/outputs/irregular_ts_ae/calorie/best.pt \
   --num_samples 4 \
   --split test
 
-python scripts/visualize_irtsae_without_input_mask_imputed.py \
+python visualize_irtsae_without_input_mask_imputed.py \
   --ckpt_path /playpen-shared/haochenz/Drift/outputs/irregular_ts_ae/heart_rate/best.pt \
   --num_samples 4 \
   --split test
 
-python scripts/visualize_irtsae_without_input_mask_imputed.py \
+python visualize_irtsae_without_input_mask_imputed.py \
   --ckpt_path /playpen-shared/haochenz/Drift/outputs/irregular_ts_ae/physical_activity/best.pt \
   --num_samples 4 \
   --split test
 
-python scripts/visualize_irtsae_without_input_mask_imputed.py \
+python visualize_irtsae_without_input_mask_imputed.py \
   --ckpt_path /playpen-shared/haochenz/Drift/outputs/irregular_ts_ae/respiratory_rate/best.pt \
   --num_samples 4 \
   --split test
